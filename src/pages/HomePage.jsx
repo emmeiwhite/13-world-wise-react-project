ea124b791878400c5f778c72c8828d33e6f5478a
emmeiwhite <emmeiwhite@gmail.com>
2024-11-02 16:37:22 +0530

    <Link to='/product'>product</Link>

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,20 @@
+import { Link } from 'react-router-dom'
+
 export default function HomePage() {
-  return <div>WorldWise</div>
+  return (
+    <div>
+      <h1>WorldWise Beautiful App</h1>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+
+      <div>
+        <Link to="/pricing">Pricing</Link>
+      </div>
+
+      <div>
+        <Link to="/product">Product</Link>
+      </div>
+    </div>
+  )
 }
