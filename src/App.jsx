729961b3c07bb9605e149cf96859eb555f8d71f2
emmeiwhite <emmeiwhite@gmail.com>
2024-11-02 16:28:45 +0530

    Page Not Found :(

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Product from './pages/product'
 import HomePage from './pages/HomePage'
 import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFound'
 
 export default function App() {
   return (
@@ -18,6 +19,11 @@ export default function App() {
         <Route
           path="pricing"
           element={<Pricing />}
+        />
+
+        <Route
+          path="*"
+          element={<PageNotFound />}
         />
       </Routes>
     </BrowserRouter>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,3 @@
 export default function Product() {
-  return <div>product</div>
+  return <div>Product</div>
 }
