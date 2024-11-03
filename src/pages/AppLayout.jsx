747a9771a85abf57dfed56f55cf9890c49f9b930
emmeiwhite <emmeiwhite@gmail.com>
2024-11-03 10:35:44 +0530

    Homepage : WIP

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,3 @@
-import AppNav from '../components/AppNav'
-
 export default function AppLayout() {
-  return (
-    <div>
-      <AppNav />
-      <h1>AppLayout Page!</h1>
-    </div>
-  )
+  return <div className="app">AppLayout</div>
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import Navbar from '../components/Navbar'
-
 export default function PageNotFound() {
   return (
     <div>
-      <Navbar />
-      <h3>Page Not Found :(</h3>
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
