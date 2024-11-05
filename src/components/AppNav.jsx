0b0b2381aee63b7a3aca90c270150fc444c3db61
emmeiwhite <emmeiwhite@gmail.com>
2024-11-05 19:23:38 +0530

    Added NavLink to AppNav.jsx component to toggle b/w sub-routes on the AppLayout.jsx component

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from 'react-router-dom'
 import styles from './AppNav.module.css'
 
 export default function AppNav() {
-  return <div className={styles.AppNav}>AppNav</div>
+  return (
+    <div className={styles.AppNav}>
+      <ul>
+        <li>
+          <NavLink to="cities">cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">countries</NavLink>
+        </li>
+      </ul>
+    </div>
+  )
 }
