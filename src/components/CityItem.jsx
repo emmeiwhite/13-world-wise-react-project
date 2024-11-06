2ad81c93dbc1bf833b1833001436193e6cc46955
emmeiwhite <emmeiwhite@gmail.com>
2024-11-06 14:40:13 +0530

    CityItem Component ready and rendering cities

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -14,7 +14,8 @@ export default function CityItem({ city }) {
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
     </li>
   )
 }
