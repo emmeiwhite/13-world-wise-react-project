6be714067d04d28361f5bdcf5ed7d1dd2f0742ca
emmeiwhite <emmeiwhite@gmail.com>
2025-02-09 16:32:25 +0530

    imran-1.1 setup basic routing with React-Router-Dom@6

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Pricing from './pages/Pricing'
+import Products from './pages/Products'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="pricing"
+          element={<Pricing />}
+        />
+
+        <Route
+          path="products"
+          element={<Products />}
+        />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App
