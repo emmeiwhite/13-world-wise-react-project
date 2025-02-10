f132cec3056d83946f7d993860ce381708c87ecd
emmeiwhite <emmeiwhite@gmail.com>
2025-02-10 14:06:30 +0530

    imran-1.6 Logo and Navlinks styled for PageNav

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from 'react-router-dom'
 import styles from './PageNav.module.css'
+import Logo from '../components/Logo'
 
 const PageNav = () => {
   return (
     <div className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
