03ed26d642cf3e23f067a2f8f1e835494797c0b9
emmeiwhite <emmeiwhite@gmail.com>
2025-02-12 04:20:26 +0530

    imran-2.5 handling the scenario when the user has not added a single city

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,12 @@
 import CityItem from './CityItem'
 import styles from './CityList.module.css'
+import Message from './Message'
 import Spinner from './Spinner'
 const CityList = ({ loading, error, cities }) => {
   if (loading) return <Spinner />
   if (error) return <p>Error: {error}</p>
+
+  if (!cities?.length) return <Message message="Add your first city by clicking on the city map" />
 
   return (
     <ul className={styles.cityList}>
