1fc3a82d0ea9e854f19227711f7b0c8c36261b15
emmeiwhite <emmeiwhite@gmail.com>
2025-02-12 04:05:48 +0530

    imran-2.3 adding cities to the sidebar component

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,5 @@
+import styles from './CityItem.module.css'
+const CityItem = ({ city }) => {
+  return <li>city</li>
+}
+export default CityItem
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,6 +1,19 @@
+import CityItem from './CityItem'
 import styles from './CityList.module.css'
+import Spinner from './Spinner'
+const CityList = ({ loading, error, cities }) => {
+  if (loading) return <Spinner />
+  if (error) return <p>Error: {error}</p>
 
-const CityList = () => {
-  return <ul className={styles.cityList}>CityList</ul>
+  return (
+    <ul className={styles.cityList}>
+      {cities.map(city => (
+        <CityItem
+          city={city}
+          key={city.id}
+        />
+      ))}
+    </ul>
+  )
 }
 export default CityList
